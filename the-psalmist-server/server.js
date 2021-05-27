621821d47278231ebf3c595e7371c9e407d2928b
Deleon Francis <dxfrancis.coding@gmail.com>
2021-05-27 17:19:23 -0400

    not cl ing hello world in the browser but cl ing in the terminal.

--- the-psalmist-server/server.js
+++ the-psalmist-server/server.js
@@ -1,10 +1,13 @@
-const env = require('dotenv').config();
-const express = require('express');
+const env = require("dotenv").config();
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.get("/home", (req,res)=>res.send("hello world"))
+app.get("/home", (req, res) => {
+  res.send("The Psalmist");
+  return console.log("Hello World");
+});
 
 const PORT = process.env.PORT || 7000;
 
-app.listen(PORT, () => console.log(`Server started on ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
