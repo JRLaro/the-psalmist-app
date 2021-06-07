d459c704691f48b4f51adf6cfacdc636adf7e9fb
Deleon Francis <dxfrancis.coding@gmail.com>
2021-06-07 12:25:30 -0400

    setting up routes for users.js post sends a string.

--- the-psalmist-server/routes/auth.js
+++ the-psalmist-server/routes/auth.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 // get logged in user
-router.get('/',(req, res)=>{
-    res.send('Get logged in user')
-})
+router.get("/", (req, res) => {
+  res.send("Get logged in user");
+});
 
 //  auth user and get token
-router.post('/', (req, res)=>{
-    res.send('log in user')
-})
+router.post("/", (req, res) => {
+  res.send("log in user");
+});
 
-module.exports = router
+module.exports = router;
