b72b40dbae309564d39241c38d2be88aa8baaa03
J.Laro <joselarojr@gmail.com>
2021-06-07 13:26:10 -0400

    refactored, addbtn, songitem list

---
+++ psalmist_frontend/src/actions/types.js
@@ -0,0 +1,9 @@
+export const GET_SONGS = "GET_SONGS";
+export const ADD_SONG = "ADD_SONG";
+export const DELETE_SONG = "DELETE_SONG";
+export const SET_CURRENT = "SET_CURRENT";
+export const CLEAR_CURRENT = "CLEAR_CURRENT";
+export const UPDATE_SONG = "UPDATE_SONG";
+export const CLEAR_SONGS = "CLEAR_SONGS";
+export const SET_LOADING = "SET_LOADING";
+export const SONGS_ERROR = "SONGS_ERROR";
---
+++ psalmist_frontend/src/songs/AddSongModal.js
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import M from "materialize-css/dist/js/materialize.min.js";
+import { addSong } from "../actions/songActions";
+
+const AddSongModal = ({ addSong }) => {
+  const [title, setTitle] = useState("");
+  const [intro, setIntro] = useState("");
+  const [verse, setVerse] = useState("");
+  const [chorus, setChorus] = useState("");
+
+const onSubmit = () => {
+  if (title === "") {
+    M.toast({ html: "What is the title of your Song?" });
+  } else {
+    const newSong = {
+      title,
+      intro,
+      verse,
+      chorus,
+      date: new Date(),
+    };
+
+    addSong(newSong);
+    M.toast({ html: `${title} was added` });
+
+    //clear fields
+    setTitle("");
+    setIntro("");
+    setVerse("");
+    setChorus("");
+  }
+}
+
+  return (
+    <div
+      id="add-song-modal"
+      className="modal"
+      style={{ width: "50%", height: "auto" }}
+    >
+      <div className="modal-content">
+        <h4>Songs</h4>
+        <br />
+        <div className="row">
+          <div className="input-field">
+            <input
+              type="text"
+              name="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <label htmlFor="title" className="active">
+              Song Title
+            </label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field">
+            <input
+              type="text"
+              name="intro"
+              value={intro}
+              onChange={(e) => setIntro(e.target.value)}
+            />
+            <label htmlFor="intro" className="active">
+              Intro
+            </label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field">
+            <input
+              type="text"
+              name="verse"
+              value={verse}
+              onChange={(e) => setVerse(e.target.value)}
+            />
+            <label htmlFor="verse" className="active">
+              Verse
+            </label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field">
+            <input
+              type="text"
+              name="chorus"
+              value={chorus}
+              onChange={(e) => setChorus(e.target.value)}
+            />
+            <label htmlFor="chorus" className="active">
+              Chorus
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="modal-footer">
+        <a
+          href="'#!"
+          onClick={onSubmit}
+          className="modal-close waves-effect green btn"
+        >
+          {" "}
+          ENTER
+        </a>
+      </div>
+    </div>
+  );
+};
+
+AddSongModal.propTypes = {
+  addSong: PropTypes.func.isRequired,
+};
+
+export default connect(null, { addSong })(AddSongModal);
