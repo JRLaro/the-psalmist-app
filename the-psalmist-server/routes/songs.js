bc2d660daa6434e10b5470d2465dcb4f44edefc0
Deleon Francis <dxfrancis.coding@gmail.com>
2021-06-07 12:30:32 -0400

    routes for songs.js sending returning a string.

---
+++ the-psalmist-server/routes/songs.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+// get user's songs
+router.get("/", (req, res) => {
+  res.send("Get user budgets");
+});
+
+// add a new song
+router.post("/", (req, res) => {
+  res.send("Add a song");
+});
+
+// update a song
+router.put("/:id", (req, res) => {
+  res.send("update a song");
+});
+
+// delete a song
+router.delete("/:id", (req, res) => {
+  res.send("delete a song");
+});
+
+module.exports = router;
