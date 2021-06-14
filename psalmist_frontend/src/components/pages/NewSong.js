1728c57d9ab39731f40ba0d6685566fc417292d9
J.Laro <joselarojr@gmail.com>
2021-06-14 16:31:38 -0400

    added (useHistory = history.push) to redirect to new_song after adding title

--- psalmist_frontend/src/components/pages/NewSong.js
+++ psalmist_frontend/src/components/pages/NewSong.js
@@ -16,11 +16,10 @@ const NewSong = ({ song: { current, song }, addSong, getSong }) => {
       } else {
         console.log("current is empty");
       }
-      
+
       // console.log(current);
     } catch (error) {
       console.log(error);
-
     }
 
     // if (current) {
@@ -64,36 +63,48 @@ const NewSong = ({ song: { current, song }, addSong, getSong }) => {
   };
 
   return (
-   <div>
-      {current ? <div className="container">
-        <h2>Song Title: {current.title}</h2>
-        <h5>Song ID: {current.id}</h5>
-        <a href="/home">
-          <button>Home</button>
-        </a>
-        <Editor
-          onInit={(evt, editor) => (editorRef.current = editor)}
-          init={{
-            height: 500,
-            menubar: false,
-            plugins: [
-              "advlist autolink lists link image charmap print preview anchor",
-              "searchreplace visualblocks code fullscreen",
-              "insertdatetime media table paste code help wordcount",
-            ],
-            toolbar:
-              "undo redo | formatselect | " +
-              "bold italic backcolor | alignleft aligncenter " +
-              "alignright alignjustify | bullist numlist outdent indent | " +
-              "removeformat | help",
-            content_style:
-              "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
-          }}
-        />
-        <a href="#!">
-          <button onClick={onSubmit}>Save Song</button>
-        </a>
-      </div>: <div>There is no data in current</div>}
+    <div>
+      {current ? (
+        <div className="container">
+          <h2>Song Title: {current.title}</h2>
+          <h5>Song ID: {current.id}</h5>
+          <a href="/home">
+            <button>Home</button>
+          </a>
+          <Editor
+            onInit={(evt, editor) => (editorRef.current = editor)}
+            init={{
+              height: 500,
+              menubar: false,
+              plugins: [
+                "advlist autolink lists link image charmap print preview anchor",
+                "searchreplace visualblocks code fullscreen",
+                "insertdatetime media table paste code help wordcount",
+              ],
+              toolbar:
+                "undo redo | formatselect | " +
+                "bold italic backcolor | alignleft aligncenter " +
+                "alignright alignjustify | bullist numlist outdent indent | " +
+                "removeformat | help",
+
+              selector: "textarea",
+              content_style:
+                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+            }}
+          />
+          <a href="#!">
+            <button onClick={onSubmit}>Save Song</button>
+          </a>
+        </div>
+      ) : (
+        <div>
+          <button>
+            <a href="/home">Home</a>
+          </button>
+          There is no data in current
+        </div>
+      )}
+        
     </div>
   );
 };
