2f97fc1f9bea64b36240ee81cf93a5ac8d2edef1
Deleon Francis <dxfrancis.coding@gmail.com>
2021-06-15 09:12:24 -0400

    started pst route for a song, completed get route.

--- the-psalmist-server/routes/songs.js
+++ the-psalmist-server/routes/songs.js
@@ -1,14 +1,45 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
+
+const auth = require("../middleware/auth");
+const User = require("../models/User");
+const Song = require("../models/Song");
 
 // get user's songs
-router.get("/", (req, res) => {
-  res.send("Get user songs");
+router.get("/", auth, async (req, res) => {
+  try {
+    const songs = await Song.find({ user: req.user.id }).sort({ date: -1 });
+    res.json(songs);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error.");
+  }
 });
 
 // add a new song
-router.post("/", (req, res) => {
-  res.send("Add a song");
+router.post("/", auth, async (req, res) => {
+  const {
+    songId,
+    title,
+    body,
+    isArchived,
+    ownerId,
+    collaborators,
+    notes,
+    songKey,
+    tempo,
+    audio,
+    dateUpdated,
+    dateCreated,
+  } = req.body
+
+  try {
+    
+    res.send("Add a song");
+  } catch (error) {
+    
+  }
 });
 
 // update a song
